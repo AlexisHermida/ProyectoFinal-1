71d34769c7670c17f3ac451b454a4e141385f4db
leandrobattocchio <leandrobattocchio@hotmail.com>
2021-11-23 14:36:17 -0300

    Nuevos botones para los mensajes

--- src/main/java/com/example/demo/controladores/AdminControlador.java
+++ src/main/java/com/example/demo/controladores/AdminControlador.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+
 @Controller
 @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 @RequestMapping("/admin")
